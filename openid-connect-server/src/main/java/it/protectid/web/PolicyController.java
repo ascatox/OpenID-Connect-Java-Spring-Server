17f6c64e17651e63d6a028bf11352c5e779ff426
ascatox <ascatox@me.com>
2019-06-12 14:55:28 +0200

    Created package it.protectid

--- openid-connect-server/src/main/java/it/protectid/web/PolicyController.java
+++ openid-connect-server/src/main/java/it/protectid/web/PolicyController.java
@@ -1,6 +1,8 @@
 package it.protectid.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author ascatox
@@ -8,16 +10,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PolicyController {
 
-	public void subscription(){
-
+	@RequestMapping(value = "/authorize", method = RequestMethod.POST)
+	public void subscription() {
+		//TODO Business Logic to implement policy emssion
 
 	}
 
+	@RequestMapping(value = "/approve", method = RequestMethod.POST)
+	public void approve() {
+		//TODO Business Logic to implement policy consensus
 
-
-
-
-
+	}
 
 
 }
